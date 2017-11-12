d8589f11c65f8ffa11689341ed638bdbad023b54
Wayne Kung <waynekung@outlook.com>
2017-11-12 16:20:31 -0500

    CreateAccount now outputs CC expiration
    Took in the String expDate and parsed it for Year and Month, then used YearMonth class to retrieve the last day of the month and converting it back to a Date object.
    
    Also refactored the "time" variable to "membershipLength"

--- src/edu/uga/cs/rentaride/logic/impl/CreateAccountCtrl.java
+++ src/edu/uga/cs/rentaride/logic/impl/CreateAccountCtrl.java
@@ -2,6 +2,9 @@ package edu.uga.cs.rentaride.logic.impl;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
@@ -33,14 +36,20 @@ public class CreateAccountCtrl {
 	    	//combining the address
 	    	address = address + ", " + city + ", " + state + ", "+ zip;
 			
-	    	//TODO
-	    	Date date1 = new Date();
-	    	long time = 24*60*60*1000;
+	    	Date ccExp = new Date();
+	    	int year = Integer.parseInt(expDate.substring(expDate.indexOf("/")+1)); // gets year after "/"
+	    	int month = Integer.parseInt(expDate.substring(0, expDate.indexOf("/"))); // gets month before "/"
+	    	YearMonth yearMonth = YearMonth.of(year, month);
+	    	LocalDate ccExpLocalDate = yearMonth.atEndOfMonth(); // gets the last day of the month
+	    	ccExpLocalDate.format(DateTimeFormatter.ISO_LOCAL_DATE); // formats it to YYYY-MM-DD
+	    	ccExp = java.sql.Date.valueOf(ccExpLocalDate); // converts LocalDate object back to Date
+	    	
+	    	long membershipLength = 24*60*60*1000;
 	    	
 	    	Date createDate = new Date();
-	    	Date membershipExpiration = new Date(createDate.getTime() + time);
+	    	Date membershipExpiration = new Date(createDate.getTime() + membershipLength);
 	    	//Passing into the object layer for create customer 
-	    	customer = objectLayer.createCustomer(fName, lName, email, password, email, address, createDate, membershipExpiration, state, driverNo, cardNo, date1);
+	    	customer = objectLayer.createCustomer(fName, lName, email, password, email, address, createDate, membershipExpiration, state, driverNo, cardNo, ccExp);
 	    	objectLayer.storeCustomer(customer);
 	    	return customer.getId();
     }
