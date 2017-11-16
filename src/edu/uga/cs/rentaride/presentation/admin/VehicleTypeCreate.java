6290c8c74d630f16defb802093616bc1c4ad7a7a
Wayne Kung <waynekung@outlook.com>
2017-11-15 19:14:07 -0500

    Removed unused imports in VehicleTypeCreate servlet

--- src/edu/uga/cs/rentaride/presentation/admin/VehicleTypeCreate.java
+++ src/edu/uga/cs/rentaride/presentation/admin/VehicleTypeCreate.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 
 import edu.uga.cs.rentaride.RARException;
 import edu.uga.cs.rentaride.entity.User;
